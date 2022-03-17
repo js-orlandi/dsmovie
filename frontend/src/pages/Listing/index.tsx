926cc358ce7c01c4a4a8041e1da57052e0506a7d
js-orlandi <js.orlandi.dev@gmail.com>
2022-03-17 15:34:10 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,47 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination"
-import { defaultMaxListeners } from "events"
 
-function Listing(){
 
-    return(
-        <Pagination />
-    )
+function Listing() {
+
+    return (
+
+        <>
+
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    
+                   
+                </div>
+            </div>
+
+
+
+
+        </>
+
+    );
+
 }
 
 export default Listing;

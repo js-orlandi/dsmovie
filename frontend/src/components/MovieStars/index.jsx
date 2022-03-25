ccda3920c16c1a9ebcc344cf14cc074c332492bd
Juliano S Orlandi <72838775+js-orlandi@users.noreply.github.com>
2022-03-25 14:25:55 -0300

    deleted defaultMaxListeners

--- frontend/src/components/MovieStars/index.jsx
+++ frontend/src/components/MovieStars/index.jsx
@@ -1,7 +1,6 @@
 import{ ReactComponent as StarFull} from 'assets/img/star-full.svg'
 import{ ReactComponent as StarHalf} from 'assets/img/star-half.svg'
 import{ ReactComponent as StarEmpty} from 'assets/img/star-empty.svg'
-import { moveEmitHelpers } from 'typescript';
 import './styles.css'
 
 function MovieStars(){
